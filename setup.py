6747b4d143b2b7b32143c6e52900c1f0dbe879e6
Horia Coman <horia141@gmail.com>
2016-07-03 19:52:01 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Secrets setup.py"""
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def readme():
+    """Long form readme for secrets."""
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
+setup(
+    name='clock',
+    version='0.0.1',
+    description='Datetime setup useful for IoC usage. Inspired by Dart-quiver\'s Clock.',
+    long_description=readme(),
+    keywords='system clock datetime time',
+    url='http://github.com/ocelot-saas/clock',
+    author='Horia Coman',
+    author_email='horia141@gmail.com',
+    license='All right reserved',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    install_requires=[
+        'pytz==2015.7',
+        # For testing
+        'tabletest3>=1,<2',
+        ],
+    test_suite='tests',
+    tests_require=[],
+    include_package_data=True,
+    zip_safe=False
+)
